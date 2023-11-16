9bdd846c89dd3ca57e2cde7535a93494c068b276
Neelmani <75542781+neelmani15@users.noreply.github.com>
2023-11-16 21:53:54 +0530

    First Commit

---
+++ client/src/component/Email.js
@@ -0,0 +1,91 @@
+import React, { useEffect,useState } from 'react'
+import { useOutletContext,useParams } from 'react-router-dom';
+import { API_URLS } from '../services/apiUrl';
+import useApi from '../hooks/useApi';
+import { Box, Typography,Checkbox,List,ListItem } from '@mui/material';
+import { DeleteOutline, Refresh,MoreVert, ArrowBackIos, ArrowForwardIos, Keyboard } from '@mui/icons-material';
+import EmailData from './EmailData';
+import NoMails from './NoMails';
+import { EMPTY_TABS, activeOptionTab } from '../constants/constant';
+import Emailtype from './Emailtype';
+
+
+export default function Email() {
+    const [activeOption, setActiveOption] = useState('Primary');
+    const [starredEmail, setStarredEmail] = useState(false);
+    const [selectedEmails, setSelectedEmails] = useState([]);
+    const {openDrawer} = useOutletContext();
+    const {type} = useParams();
+    const getEmailsService = useApi(API_URLS.getEmailFromType);
+    const deleteEmailsService = useApi(API_URLS.deleteEmails);
+    const moveEmailsToBin = useApi(API_URLS.moveEmailsToBin);
+    useEffect(()=>{
+        getEmailsService.call({},type);
+    },[type,starredEmail]);
+
+    const selectAllEmails = (e) => {
+        if (e.target.checked) {
+            const emails = getEmailsService?.response?.map(email => email._id);
+            setSelectedEmails(emails);
+        } else {
+            setSelectedEmails([]);
+        }
+    }
+    const deleteSelectedEmails = () => {
+        if (type === 'bin') {
+            deleteEmailsService.call(selectedEmails);
+        } else {
+            moveEmailsToBin.call(selectedEmails);
+        }
+        setStarredEmail(prevState => !prevState);
+    }
+    const handleOptionClick = (option) => {
+        setActiveOption(option);
+      };
+  return (
+    <div style={openDrawer ? {marginLeft:250,width:'calc(100%-250px)'}:{width:'100%'}}>
+        <Box style={{padding:'10px 10px 0 10px',display:'flex',alignItems:'center',justifyContent:'space-between'}}>
+            <Box style={{display:'flex',alignItems:'center'}}>
+                <Checkbox size='small' onChange={(e) => selectAllEmails(e)} />
+                <Refresh fontSize='small' style={{padding:'0 3px'}} />
+                <DeleteOutline fontSize='small' style={{padding:'0 3px'}} onClick={(e) => deleteSelectedEmails(e)} />
+                <MoreVert fontSize='small' style={{padding:'0 3px'}} />
+            </Box>
+            <Box style={{display:'flex',alignItems:'center',marginRight:15}}>
+                <Typography style={{padding:'0 12px',fontSize:12}}>1-50 of 159</Typography>
+                <ArrowBackIos fontSize='13px' style={{padding:'0 3px',color:'gray'}} />
+                <ArrowForwardIos fontSize='13px' style={{padding:'0 3px'}} />
+                <Keyboard fontSize='15px' style={{padding:'0 3px'}} />
+            </Box>
+        </Box>
+        <Emailtype activeOption={activeOption} handleOptionClick={handleOptionClick} />
+        { (activeOption === 'Social') &&
+            // <div style={{display:'flex',alignItems:'center',flex: '1',marginTop:100,marginLeft:200 }}>There are no mails to display</div>
+            <NoMails message={activeOptionTab['Social']}/>
+        }
+        { (activeOption === 'Promotions') &&
+            // <div style={{display:'flex',alignItems:'center',flex: '1',marginTop:100,marginLeft:200 }}>There are no mails to display</div>
+            <NoMails message={activeOptionTab['Promotions']}/>
+        }
+        { activeOption === "Primary" &&
+            <List>
+                {
+                    getEmailsService?.response?.map(email=>(
+                        <EmailData 
+                            key={email._id}
+                            email={email}
+                            setStarredEmail={setStarredEmail}
+                            selectedEmails={selectedEmails}
+                            setSelectedEmails={setSelectedEmails}
+                        />
+                    ))
+                }
+            </List>
+        }
+            {
+                    getEmailsService?.response?.length === 0 &&
+                    <NoMails message={EMPTY_TABS[type]} />
+            }
+    </div>
+  )
+}
---
+++ client/src/constants/constant.js
@@ -0,0 +1,46 @@
+export const gmailLogo = 'https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png';
+export const emptyProfilePic = 'https://ssl.gstatic.com/ui/v1/icons/mail/profile_mask2.png';
+
+export const VIEWS = {
+    inbox: 'inbox',
+    starred: 'starred',
+    sent: 'sent',
+    drafts: 'drafts'
+}
+
+export const EMPTY_TABS = {
+    inbox: {
+        heading: 'Your inbox is empty',
+        subHeading: "Mails that don't appear in other tabs will be shown here."
+    },
+    drafts: {
+        heading: "You don't have any saved drafts.",
+        subHeading: "Saving a draft allows you to keep a message you aren't ready to send yet."
+    },
+    starred: {
+        heading: 'No starred messages',
+        subHeading: "Stars let you give messages a special status to make them easier to find. To star a message, click on the star outline beside any message or conversation."
+    },
+    sent: {
+        heading: 'No sent messages!',
+        subHeading: 'Send one now!'
+    },
+    bin: {
+        heading: 'No conversations in Bin.',
+        subHeading: ''
+    },
+    allmail:{
+        heading:'No messages here',
+        subHeading:""
+    }
+}
+
+export const activeOptionTab = {
+    'Social':{
+        heading: 'There are no social mails to display.',
+        subHeading:""
+    },
+    'Promotions':{
+        heading:"There are no promotions mails to display."
+    }
+}
